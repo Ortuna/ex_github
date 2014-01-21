120f51bd1db1124721fae1c4a1e520b62a85af5d
Sumeet Singh <ortuna@gmail.com>
2014-01-21 13:34:29 -0500

    Fixed up custom ExVCR

--- test/ex_github/content_test.exs
+++ test/ex_github/content_test.exs
@@ -4,12 +4,12 @@ defmodule ContentTest do
   alias ExGithub.Repo.Content
 
   setup_all do
-    ExVCR.Config.cassette_library_dir("test/fixture/vcr_cassettes")
+    ExVCR.Config.cassette_library_dir("test/fixture/vcr_cassettes", "test/fixture/vcr_cassettes")
     :ok
   end
 
   test "can get a remote manifest" do
-    use_cassette "manifest" do
+    use_cassette "manifest", custom: true do
       json = Content.fetch_content(owner: "Ortuna", repo: "progit-bana", path: "manifest.json")
       assert json["decoded_content"] == "{\n    \"title\": \"Pro Git\",\n    \"cover_image\": \"assets/cover.jpg\",\n    \"pages\":\n    {\n        \"Preface\": \"01-introduction/01-chapter1.markdown\"\n    }\n}\n"
     end
